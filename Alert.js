d007ed201f9ff046489c8f6377d6f2ad70f978c8
Drynae <114296730+Drynae@users.noreply.github.com>
2023-02-13 23:15:25 +0800

    Add files via upload

---
+++ Alert.js
@@ -0,0 +1,4 @@
+alert("YOU GOT AN INVITATION");
+alert("WHEN: February 14th, 8PM");
+alert("WHERE: GMEET")
+alert("What to Prepare: Laptop, Wear anything that is comfortable for you, and kagandahan mo ")
